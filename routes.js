795732f6357f5d5eb94d2ec5117c87b51e352305
FaisITApps <itapp5.sti@gmail.com>
2021-05-09 20:34:39 +0700

    perbaharui add query

--- routes.js
+++ routes.js
@@ -1,3 +1,18 @@
+// Detailed notation
+const handler = (request, h) => {
+    const response = h.response('success');
+    response.type('text/plain');
+    response.header('X-Custom', 'some-value');
+    return response;
+};
+
+// Chained notation
+const handler = (request, h) => {
+    return h.response('success')
+        .type('text/plain')
+        .header('X-Custom', 'some-value');
+};
+
 const routes = [
     {
         method: 'GET',
@@ -34,6 +49,36 @@ const routes = [
             return 'Halaman tidak ditemukan';
         },
     },
-];
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const {name} = request.params;
+
+            const { lang } = request.query;
  
+            if(lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+
+            return `Hello ${name}`;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}!`;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/user',
+        handler: (request, h) => {
+        return h.response('created').code(201);
+        }
+    },
+];
+
 module.exports = routes;
